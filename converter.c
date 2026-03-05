e55be60261b49c12a7e580306bd55501a2835e9d
Noam790 <noamchamouard@outlook.fr>
2026-03-05 13:45:59 +0100

    fixed some bugs

--- converter.c
+++ converter.c
@@ -3,37 +3,48 @@
 #include <math.h>
 #include <string.h>
 
-char *itos(int nb){
-    int size = (int)((ceil(log10(nb))+1)*sizeof(char));
-    char *nbtos = malloc(size);
+// convert a  number to its ascii val
+char itos(int nb){
+    return (nb >= 0 && nb <= 9) ? (char)(nb + '0') : (char)(nb - 10 + 'A');
+}
 
-    sprintf(nbtos, "%d", nb);
+char *reverse(char *s) {
+    int n = strlen(s);
+    char *res = (char *)malloc((n + 1) * sizeof(char));
 
-    return nbtos;
+    int j = 0;
+    
+    for (int i = n - 1; i >= 0; i--) {
+        res[j] = s[i];
+        j++;
+    }
+
+    res[n] = '\0';
+    return res;
 }
 
 int sizeNeeded(int b1, int b2){
-    char *b1_tostr = itos(b1);
-
-    int len_b1 = strlen(b1_tostr);
+    int len_b1 = floor(log10(abs(b1))) + 1;
     int size_convertion = len_b1 * log(b1)/log(b2);
 
     return size_convertion;
 }
 
-char *convertTo(int destination, int base){
-    char *arr = malloc(sizeNeeded(base, destination));
-
-    while(base > 0){
-        char *res = itos(base % destination);
-        printf("%s\n", res);
-        strcat(arr, res);
-        base = round(base / destination);
+// convert a decimal number to the defined base
+char *convertTo(int number, int base){
+    char *arr = malloc(sizeNeeded(base, number));
+    printf("%ld", sizeof(arr));
+    while(number > 0){
+        char res = itos(number % base);
+        sprintf(arr++, "%c", res);
+        number /= base;
     }
-    
+
+    *arr = '\0';
     return arr;
 }
 
+// convert a number from any base to decimal
 int convertFrom(char *digits, int base){
     int n = 0;
 
@@ -46,7 +57,8 @@ int convertFrom(char *digits, int base){
 
 // graphical interface
 int main() {
-    printf("%d\n", convertFrom("11012", 3));
-    printf("%s\n", convertTo(7, 2));
+    //printf("%d\n", convertFrom("1102A", 16));
+    printf("%s\n", convertTo(282, 16));
+    printf("%c\n",itos(5));
     return 0;
 }
