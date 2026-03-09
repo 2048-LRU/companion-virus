ee10eca02824bbbe1e647c71ec53099664cca979
Noam <noamchamouard@outlook.fr>
2026-03-09 17:57:34 +0100

    optimisations and minor fixes

--- src/utils/converter.c
+++ src/utils/converter.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <assert.h>
 
 // convert a number to its ascii value
 char itos(int nb) {
@@ -14,6 +15,7 @@ int stoi(char c) {
 }
 
 size_t sizeNeeded(int num, int base) {
+    if (num == 0) return 2;
     size_t len = floor(log(abs(num)) / log(base)) + 1;
 
     return len + 1;
@@ -40,6 +42,17 @@ char *reverse(char *s) {
 
 // convert a decimal number to the defined base
 char *convertDecimalTo(int number, int base) {
+    if (number == 0) {
+        char *res = malloc(2);
+        if (res == NULL) {
+            fprintf(stderr, "Allocation failed\n");
+            return NULL;
+        }
+
+        res[0] = '0';
+        res[1] = '\0';
+    }
+
     if (base < 2 || base > 36) {
         fprintf(stderr, "Invalid base\n");
         return NULL;
@@ -75,8 +88,16 @@ int convertDecimalFrom(char *digits, int base) {
     }
 
     int n = 0;
-    for (int d = 0; d < strlen(digits); d++) {
-        n = (base * n) + stoi(digits[d]);
+    size_t len = strlen(digits);
+    for (int d = 0; d < len; d++) {
+
+        int val = stoi(digits[d]);
+        if (val >= base) {
+            fprintf(stderr, "Invalid digit for base\n");
+            return -1;
+        }
+
+        n = (base * n) + val;
     }
 
     return n;
@@ -84,19 +105,34 @@ int convertDecimalFrom(char *digits, int base) {
 
 // graphical interface
 int main() {
-    printf("%s\n", convertDecimalTo(282, 2));   // Result: 100011010
-    printf("%s\n", convertDecimalTo(282, 8));   // Result: 432
-    printf("%s\n", convertDecimalTo(282, 16));  // Result: 11A
-    printf("%s\n", convertDecimalTo(282, 36));  // Result: 7U
-    printf("%p\n", convertDecimalTo(282, 1));   // Error: Invalid base
+    char *res;
+    res = convertDecimalTo(282, 2);
+    assert(res != NULL);
+    assert(strcmp(res, "100011010") == 0);
+    free(res);
 
-    printf("\n");
+    res = convertDecimalTo(282, 8);
+    assert(res != NULL);
+    assert(strcmp(res, "432") == 0);
+    free(res);
 
-    printf("%d\n", convertDecimalFrom("100011010", 2));  // Result: 282
-    printf("%d\n", convertDecimalFrom("432", 8));        // Result: 282
-    printf("%d\n", convertDecimalFrom("11A", 16));       // Result: 282
-    printf("%d\n", convertDecimalFrom("7U", 36));        // Result: 282
-    printf("%d\n", convertDecimalFrom("100011010", 1));  // Error: Invalid base
+    res = convertDecimalTo(282, 16);
+    assert(res != NULL);
+    assert(strcmp(res, "11A") == 0);
+    free(res);
+
+    res = convertDecimalTo(282, 36);
+    assert(res != NULL);
+    assert(strcmp(res, "7U") == 0);
+    free(res);
+
+    assert(convertDecimalTo(282, 1) == NULL); // Invalid base
+
+    assert(convertDecimalFrom("100011010", 2) == 282);
+    assert(convertDecimalFrom("432", 8) == 282);
+    assert(convertDecimalFrom("11A", 16) == 282);
+    assert(convertDecimalFrom("7U", 36) == 282);
+    assert(convertDecimalFrom("100011010", 1) == -1); // Invalid base
 
     return 0;
 }
