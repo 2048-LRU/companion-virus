bef7973cfad9306f7239e223f4e6eb407dc62c3d
Noam790 <noamchamouard@outlook.fr>
2026-03-21 10:04:26 +0100

    docker introuvable sur les vms

--- launch.sh
+++ launch.sh
@@ -2,18 +2,13 @@
 
 set -e
 
-docker build -f .devcontainer/Dockerfile -t companion-virus:latest .
+sudo apt-get update
+sudo apt-get install -y --no-install-recommends \
+    build-essential cmake pkg-config \
+    libgtk-4-dev libglib2.0-dev libgio-2.0-0 \
+    libssl-dev
 
-docker run -it --rm \
-    --name companion-virus-test \
-    -v "$(pwd)":/workspace \
-    -w /workspace \
-    companion-virus:latest \
-    bash -c "
-        mkdir -p build
-        cd build
-        cmake ..
-        make
-        bash ../setup_test_env.sh
-        cd TPUser
-        /bin/bash"
+mkdir -p build && cd build
+cmake ..
+make
+bash ../setup_test_env.sh
